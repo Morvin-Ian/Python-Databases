b4126792f6cabb2c497a37e0237f4ae812d2dce1
Morvin-Ian <morvinian@gmail.com>
2022-07-12 14:15:34 +0300

    mongo

--- mongodb.py
+++ mongodb.py
@@ -1,1 +1,24 @@
 # pip install pymongo
+# pip install dnspython
+
+import pymongo
+from pymongo import MongoClient
+
+
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/myFirstDatabase"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client['user_shopping_list']
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+
+    # Get the database
+    dbname = get_database()
